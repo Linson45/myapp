a3016b9c5b419da6ceb7aa816d16b0329f167853
yogeeta <yogeeta@opspl>
2020-07-21 14:08:01 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import QrReader from "react-qr-reader";
+import "antd/dist/antd.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { Input, Modal, Button } from "antd";
+class App extends React.Component {
+  state = { visible: false, result: "No result" };
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+  handleScan = (data) => {
+    if (data) {
+      this.setState({
+        result: data,
+      });
+    }
+  };
+  handleError = (err) => {
+    console.error(err);
+  };
+
+  render() {
+    return (
+      <>
+        <Input placeholder="click to scan " onClick={this.showModal} />
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <QrReader
+            delay={300}
+            onError={this.handleError}
+            onScan={this.handleScan}
+            style={{ width: "100%" }}
+          />
+          <p>{this.state.result}</p>
+        </Modal>
+      </>
+    );
+  }
 }
+// function showmodal() {
+//   return (
+//     <Modal
+//       title="Basic Modal"
+//       visible={this.state.visible}
+//       onOk={this.handleOk}
+//       onCancel={this.handleCancel}
+//     >
+//       <p>Test</p>
+//     </Modal>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Input placeholder="Basic usage" onChange={showmodal} />
+//     </div>
+//   );
+// }
 
 export default App;
